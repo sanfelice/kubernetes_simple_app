781a1692f8085a36f90778db3407ee8eb92eb100
Ricardo Sanfelice <ricardosanfelice@gmail.com>
2022-11-08 20:26:06 +0100

    Initial commit

---
+++ src/test/java/com/sanfelice/techtalk/kubernetes/KubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanfelice.techtalk.kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
